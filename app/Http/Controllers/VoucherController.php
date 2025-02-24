8f51941cb61e0b44b00ab22685926121a6b1802f
StevenCesen <steven.cesen@unl.edu.ec>
2025-02-23 20:45:40 -0500

    Cambio para traer identificacion del contribuyente

--- app/Http/Controllers/VoucherController.php
+++ app/Http/Controllers/VoucherController.php
@@ -215,6 +215,7 @@ class VoucherController extends Controller
     public function show(Voucher $id)
     {
         $id->client=$id->find($id->id)->client;
+        $id->contributor_identification=$id->find($id->id)->contributor()->identification;
         return $id;
     }
 
