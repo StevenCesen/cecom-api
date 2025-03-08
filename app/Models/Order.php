fcc3785fb26fb4a543e25ebba53fb98e24bc95a5
StevenCesen <steven.cesen@unl.edu.ec>
2025-03-08 11:50:13 -0500

    Se agrego la visualización de orden

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -33,7 +33,7 @@ class Order extends Model
     }
 
     public function establisment(){
-        return $this->hasOne(Establishment::class,'user_id');
+        return $this->hasOne(Establishment::class,'user_id','user_id');
     }
     
     public function itemcart(){
