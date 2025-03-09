fa0f23b97ea24ca629a3e6fb84aa1cd67d729884
StevenCesen <steven.cesen@unl.edu.ec>
2025-03-09 01:51:24 -0500

    Se agrego finalizado de orden

--- resources/views/ride.blade.php
+++ resources/views/ride.blade.php
@@ -227,13 +227,13 @@
         </tr>
         @foreach(json_decode($items) as $item)
             <tr>
-                <td>{{ $item->cantidad }}</td>
-                <td>{{ $item->codigo }}</td>
-                <td style="text-align: left;word-wrap: break-word;width:100%;">{{ $item->nombre }}</td>
-                <td style="text-align: left;word-wrap: break-word;width:100%;">{{ $item->descripcion }}</td>
-                <td>{{ $item->precio_unitario }}</td>
-                <td>{{ $item->descuento }}</td>
-                <td>{{ $item->importe }}</td>
+                <td style="text-align: left;word-wrap: break-word;width:5%;">{{ $item->cantidad }}</td>
+                <td style="text-align: left;word-wrap: break-word;width:10%;">{{ $item->codigo }}</td>
+                <td style="text-align: left;word-wrap: break-word;width:25%;">{{ $item->nombre }}</td>
+                <td style="text-align: left;word-wrap: break-word;width:35%;">{{ $item->descripcion }}</td>
+                <td style="text-align: left;word-wrap: break-word;width:8.33%;">{{ $item->precio_unitario }}</td>
+                <td style="text-align: left;word-wrap: break-word;width:8.33%;">{{ $item->descuento }}</td>
+                <td style="text-align: left;word-wrap: break-word;width:8.33%;">{{ $item->importe }}</td>
             </tr>
         @endforeach
     </table>
